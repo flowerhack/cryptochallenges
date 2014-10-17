1488209a0ea2fe50710eff45e314808a1d2174f4
flowerhack <flowerhack@azureye.net>
2014-10-17 03:21:08 -0400

    some cleanup

--- cryptopals.py
+++ cryptopals.py
@@ -9,51 +9,50 @@ import codecs
 decode_hex = codecs.getdecoder("hex_codec")
 encode_hex = codecs.getencoder("hex_codec")
 
-# --------- Utility Functions ---------
+# ------------------ Utility Functions ------------------
+
 
 def _string_from_file(infile):
-	"""
-	Converts the contents of infile into a single string, sans newlines
-	"""
-	text = ""
-	with open(infile) as f:
-		text = "".join(line.strip() for line in f)
-	return text
+    """
+    Converts the contents of infile into a single string, sans newlines
+    """
+    text = ""
+    with open(infile) as f:
+        text = "".join(line.strip() for line in f)
+    return text
+
 
 def _display_hex(raw_hex_str):
-	"""
-	Python's `hex` function gives us a hex-formatted string, but it leaves a 
-	`0x` on the front, and if it's a long, it leaves an `L` on the end.  So, 
-	we strip those out here
-	"""
-	return hex(raw_hex_str)[2:].replace("L","")
+    """
+    Python's `hex` function gives us a hex-formatted string, but it leaves a
+    `0x` on the front, and if it's a long, it leaves an `L` on the end.  So,
+    we strip those out here
+    """
+    return hex(raw_hex_str)[2:].replace("L", "")
+
 
 def _score_chars(word):
-	"""
-	Assigns a integer 'score' to a string.
-	A higher score indicates it is more likely to be an English phrase.
-	"""
-	score = 0
-	for char in word:
-		if re.match("[A-Za-z ]", char):
-			score = score+1
-	return score
+    """
+    Assigns a integer 'score' to a string.
+    A higher score indicates it is more likely to be an English phrase.
+    """
+    score = 0
+    for char in word:
+        if re.match("[A-Za-z ]", char):
+            score = score+1
+    return score
 
+
+# TODO clean this one up
 def hamming_distance(string1, string2):
-	"""
-	Finds the bitwise hamming distance between two b64-encoded strings.
-	"""
-	diffs = 0
-	#string1 = base64.b64decode(string1)
-	#string2 = base64.b64decode(string2)
-	for x, y in zip (string1, string2):
-		# was x.encode("hex")
-		# "t" --> hex --> int
-		# before it was "t".encode("hex") got us to hex, but we can't convert "t" straight to hex
-		# so we need 
-		# "t" --> bytes --> hex --> int
-		diffs = diffs + bin((int(encode_hex(bytes(x, 'utf-8'))[0], 16)^int(encode_hex(bytes(y, 'utf-8'))[0], 16))).count('1')
-	return diffs / 1.0
+    """
+    Finds the bitwise hamming distance between two b64-encoded strings.
+    """
+    diffs = 0
+    for x, y in zip (string1, string2):
+        diffs = diffs + bin((int(encode_hex(bytes(x, 'utf-8'))[0], 16) ^ int(encode_hex(bytes(y, 'utf-8'))[0], 16))).count('1')
+    return diffs / 1.0
+
 
 # From the pydocs: https://docs.python.org/3/library/itertools.html#itertools-recipes
 def grouper(iterable, n, fillvalue=None):
@@ -62,229 +61,225 @@ def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
 
-# --------- Main Functions ---------
+# ------------------ Main Functions ------------------
+
 
 def hex_to_b64(hex_str):
-	""" Converts a hex-formatted string into b64 bytes """
-	return base64.b64encode(decode_hex(hex_str)[0])
+    """ Converts a hex-formatted string into b64 bytes """
+    return base64.b64encode(decode_hex(hex_str)[0])
+
 
 def xor_two_buffers(buffer1, buffer2):
-	""" Takes two hex-formatted strings and returns a hex-formatted string """
-	return _display_hex(int(buffer1, 16) ^ int(buffer2, 16))
+    """ Takes two hex-formatted strings and returns a hex-formatted string """
+    return _display_hex(int(buffer1, 16) ^ int(buffer2, 16))
+
 
 # TODO squash this into the previous method
 def xor_two_buffers_mod(buffer1, buffer2):
-	""" Takes a string and a bytes, xors them together, and returns bytes """
-	result = []
-	for i, k in zip(buffer1, list(buffer2)):
-		result.append(i^k)
-	return bytes(result)
+    """ Takes a string and a bytes, xors them together, and returns bytes """
+    result = []
+    for i, k in zip(buffer1, list(buffer2)):
+        result.append(i ^ k)
+    return bytes(result)
+
 
 def decode_single_byte_xor_cypher(hex_str, retkey=False):
-	"""
-	Takes a hex-formatted string which has been encrypted with single-byte xor.
-	Returns the decrypted string.
-	If retkey is True, we return [decrypted_string, key_used_to_decrypt]
-	"""
-	max_score = 0
-	best_match = ""
-	for key in range(255):
-		xor_result = [chr(byte ^ key) for byte in list(decode_hex(hex_str)[0])]
-		xor_result = ''.join(xor_result)
-		score = _score_chars(xor_result)
-		if score > max_score:
-			max_score = score
-			best_match = xor_result
-			best_key = key
-	if retkey:
-		return [best_match, best_key]
-	else:
-		return best_match
+    """
+    Takes a hex-formatted string which has been encrypted with single-byte xor.
+    Returns the decrypted string.
+    If retkey is True, we return [decrypted_string, key_used_to_decrypt]
+    """
+    max_score = 0
+    best_match = ""
+    for key in range(255):
+        xor_result = [chr(byte ^ key) for byte in list(decode_hex(hex_str)[0])]
+        xor_result = ''.join(xor_result)
+        score = _score_chars(xor_result)
+        if score > max_score:
+            max_score = score
+            best_match = xor_result
+            best_key = key
+    if retkey:
+        return [best_match, best_key]
+    else:
+        return best_match
+
 
 def detect_single_byte_xor_cypher(infile):
-	"""
-	Given a file of hex-formatted strings, determine which one has been 
-	encrypted with single-byte xor and return it 
-	"""
-	max_score = 0
-	best_match = ""
-	with open(infile) as f:
-		for line in f:
-			xor_result = decode_single_byte_xor_cypher(line.rstrip())
-			score = _score_chars(xor_result)
-			if score > max_score:
-				max_score = score
-				best_match = xor_result
-	return best_match.rstrip()
+    """
+    Given a file of hex-formatted strings, determine which one has been
+    encrypted with single-byte xor and return it
+    """
+    max_score = 0
+    best_match = ""
+    with open(infile) as f:
+        for line in f:
+            xor_result = decode_single_byte_xor_cypher(line.rstrip())
+            score = _score_chars(xor_result)
+            if score > max_score:
+                max_score = score
+                best_match = xor_result
+    return best_match.rstrip()
+
 
 def repeating_key_xor_encrypt(plaintext, key):
-	"""
-	Given plaintext and key, uses key to encrypt plaintext via repeating XOR
-	and returns the result
-	"""
-	keychargen = cycle(key)
-	result = ""
-	for plainchar in plaintext:
-		# Plainchar is a unicode string.  We get the ascii value of that representation,
-		# then XOR it against the next value in the key iterator.  Then we find out what the
-		# resultant character is.
-		result = result + str(ord(plainchar) ^ ord(next(keychargen)))
-	return result
+    """
+    Given plaintext and key, uses key to encrypt plaintext via repeating XOR
+    and returns the result
+    """
+    keychargen = cycle(key)
+    result = ""
+    for plainchar in plaintext:
+        # Plainchar is a unicode string.  We get the ascii value of that representation,
+        # then XOR it against the next value in the key iterator.  Then we find out what the
+        # resultant character is.
+        result = result + str(ord(plainchar) ^ ord(next(keychargen)))
+    return result
+
 
 def guess_keylength(cyphertext):
-	smallest_so_far = 999999999999999999999
-	true_keylen = None
-	for keysize in range(2, 100):  # Test every key size.
-		chunks = []
-		normalized_distances = []
-		# For each potential key, get four chunks o' bytes that are the size of the key.
-		for keychunknum in range(4):
-			index = keysize*keychunknum
-			chunks.append([cyphertext[x] for x in range(index, index+keysize)])
+    smallest_so_far = 999999999999999999999
+    true_keylen = None
+    for keysize in range(2, 100):  # Test every key size.
+        chunks = []
+        normalized_distances = []
+        # For each potential key, get four chunks o' bytes that are the size of the key.
+        for keychunknum in range(4):
+            index = keysize*keychunknum
+            chunks.append([cyphertext[x] for x in range(index, index+keysize)])
 
-		for i in range(3):
-			norm_dist = hamming_distance(''.join(chunks[i]), ''.join(chunks[i+1]))
-			normalized_distances.append(norm_dist)  # To self: is the b64 stuff going to screw anything up?
-		avg = float(sum(normalized_distances))/len(normalized_distances)/keysize
-		print(avg)
-		if avg < smallest_so_far:
-			smallest_so_far = avg
-			true_keylen = keysize
-	return true_keylen
+        for i in range(3):
+            norm_dist = hamming_distance(''.join(chunks[i]), ''.join(chunks[i+1]))
+            normalized_distances.append(norm_dist)  # To self: is the b64 stuff going to screw anything up?
+        avg = float(sum(normalized_distances))/len(normalized_distances)/keysize
+        print(avg)
+        if avg < smallest_so_far:
+            smallest_so_far = avg
+            true_keylen = keysize
+    return true_keylen
 
+
+# This is a "lazy" vignere decrypter; it's using brute force.
+# TODO come back and make it less brute-force-y
 def decrypt_vigenere(infile):
-	cyphertext = _string_from_file(infile)
-	cyphertext = base64.b64decode(cyphertext)
-	# keylength = guess_keylength(cyphertext)
-	# LOL LET'S MEGA BRUTE FORCE INSTEAD
-	max_score = 0
-	best_keylen = 0
-	best_key = ""
-	for keylength in range(2, 40):
+    cyphertext = _string_from_file(infile)
+    cyphertext = base64.b64decode(cyphertext)
+    max_score = 0
+    best_keylen = 0
+    best_key = ""
+    for keylength in range(2, 40):
 
-		# We want to create a block that contains only the first byte of
-		# every block, the second byte of every block, etc, up to KEYLENGTH.
-		blocks = defaultdict(str)
-		keylength_cycle = cycle(range(keylength))
-		for byte in cyphertext:
-			index = next(keylength_cycle)
-			blocks[index] = blocks[index] + byte
-		decoded_chunked = ""
-		
-		totalkey = ""
-		for i in range(keylength):
-			value = decode_single_byte_xor_cypher(blocks[i].encode("hex"), retkey=True)
-			decoded_text_chunk = value[0]
-			totalkey = totalkey + chr(value[1])
-			decoded_chunked = decoded_chunked + decoded_text_chunk
-			if _score_chars(decoded_chunked) > max_score:
-				max_score = score_chars(decoded_chunked)
-				best_keylen = keylength
-				best_key = totalkey
-		#print str(keylength) + " score: " + str(score_chars(decoded_chunked)) + " key: " + str(best_key)
-	# We now have the best key.
-	# Use it to decrypt the file.
-	print("Winning")
-	best_key_cycle = cycle(best_key)
-	for byte in cyphertext:
-		sys.stdout.write(chr(ord(byte) ^ ord(next(best_key_cycle))))
+        # We want to create a block that contains only the first byte of
+        # every block, the second byte of every block, etc, up to KEYLENGTH.
+        blocks = defaultdict(str)
+        keylength_cycle = cycle(range(keylength))
+        for byte in cyphertext:
+            index = next(keylength_cycle)
+            blocks[index] = blocks[index] + byte
+        decoded_chunked = ""
+        
+        totalkey = ""
+        for i in range(keylength):
+            value = decode_single_byte_xor_cypher(blocks[i].encode("hex"), retkey=True)
+            decoded_text_chunk = value[0]
+            totalkey = totalkey + chr(value[1])
+            decoded_chunked = decoded_chunked + decoded_text_chunk
+            if _score_chars(decoded_chunked) > max_score:
+                max_score = score_chars(decoded_chunked)
+                best_keylen = keylength
+                best_key = totalkey
 
-# ecb = electronic codebook. doesn't hide data patterns well
-# aes: advanced encryption standard
-# aside: what characters are allowed in python function definitions? why?
-# TODO remove as dup
-def decrypt_aes_128_in_ecb_mode(infile, key):
-	cyphertext = _string_from_file(infile)
-	cyphertext = base64.b64decode(cyphertext)
-	key = "YELLOW SUBMARINE"
-	return AES.new(key).decrypt(cyphertext)
+    # We now have the best key.
+    # Use it to decrypt the file.
+    best_key_cycle = cycle(best_key)
+    for byte in cyphertext:
+        sys.stdout.write(chr(ord(byte) ^ ord(next(best_key_cycle))))
+
 
 def aes_128_in_ecb_mode(infile, key, action, from_string=False, from_b64=True):
-	"""
-	Allows encrypt or decryption via AES-128 in ECB mode
-	If action=="encrypt", this encrypts the contents of infile with key and returns the result.
-	If action=="decrypt", this instead returns the *decrypted* contents of infile.
-	Else, returns None.
-	"""
-	cyphertext = infile if from_string else _string_from_file(infile)
-	cyphertext = cyphertext if not from_b64 else base64.b64decode(cyphertext)
-	key = "YELLOW SUBMARINE"
-	if action == "encrypt":
-		return AES.new(key).encrypt(cyphertext)
-	elif action == "decrypt":
-		return AES.new(key).decrypt(cyphertext)
-	else:
-		return
+    """
+    Allows encrypt or decryption via AES-128 in ECB mode
+    If action=="encrypt", this encrypts the contents of infile with key and returns the result.
+    If action=="decrypt", this instead returns the *decrypted* contents of infile.
+    Else, returns None.
+    """
+    cyphertext = infile if from_string else _string_from_file(infile)
+    cyphertext = cyphertext if not from_b64 else base64.b64decode(cyphertext)
+    key = "YELLOW SUBMARINE"
+    if action == "encrypt":
+        return AES.new(key).encrypt(cyphertext)
+    elif action == "decrypt":
+        return AES.new(key).decrypt(cyphertext)
+    else:
+        return
+
 
 def detect_aes_128_in_ecb_mode(infile):
-	"""
-	Detects which line in infile has been encrypted with AES-128 in ECB mode 
-	and returns that line.
+    """
+    Detects which line in `infile` has been encrypted with AES-128 in ECB mode
+    and returns that line.
 
-	Note: this returns the FIRST probable ciphered line, not ALL probable 
-	ciphered lines.
-	"""
-	with open(infile) as f:
-		for i, line in enumerate(f):
-			cyphertext = decode_hex(line.strip())[0]
-			# Problem hinted at doing 16 bytes at a time, so let's try that
-			sets = []
-			for group in grouper(cyphertext, 16):
-				sets.append(group)
-			if len(set(sets)) < len(sets):
-				return ("Line " + str(i) + ": " + str(line.strip()))
+    Note: this returns the FIRST probable ciphered line, not ALL probable
+    ciphered lines.
+    """
+    with open(infile) as f:
+        for i, line in enumerate(f):
+            cyphertext = decode_hex(line.strip())[0]
+            # Problem hinted at doing 16 bytes at a time, so let's try that
+            sets = []
+            for group in grouper(cyphertext, 16):
+                sets.append(group)
+            if len(set(sets)) < len(sets):
+                return ("Line " + str(i) + ": " + str(line.strip()))
+
 
 def pkcs7_padding(utf8_string, target_length):
-	# are strings utf-8 by default?
-	bytetext = bytearray(bytes(utf8_string, "utf-8"))
-	# is range FIXED here or is it recalculated each time?
-	# diff between "append" and "extend" for bytearray?
-	for i in range(len(bytetext), target_length):
-		bytetext.extend(b'\x04')
-	assert(len(bytetext) == target_length)
-	return bytetext.decode('utf-8')
+    bytetext = bytearray(bytes(utf8_string, "utf-8"))
+    for i in range(len(bytetext), target_length):
+        bytetext.extend(b'\x04')
+    assert(len(bytetext) == target_length)
+    return bytetext.decode('utf-8')
 
-# each ciphertext block is *added* to the next plaintext block before the next call to the cipher core
-# first plaintext block: has no previous ciphertext block. it's added to the IV
-# each plaintext block is XORed with the previous ciphertext block before being encrypted
+
+# I found the diagram on the CBC wikipedia article very helpful for this!
+# https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher-block_chaining_.28CBC.29
+# TODO clean this up wow it's super ugly and I bet we're using it again too
 def cbc_mode(infile, key, iv, action, from_string=False, is_b64=True):
-	# call ecb: make it encrypt instead of decrypt
-	# use XOR function to combine them
-	# ...????
-	# First block: plaintext & XOR against IV THEN encrypt that
-	#import pdb; pdb.set_trace()
-	text = infile if from_string else _string_from_file(infile)
-	keysize = len(key)
-	iv = iv * keysize  # TODO mod iv
-	text = base64.b64decode(text) if is_b64 else text
-	prev_block = None
-	modtext = b''
+    text = infile if from_string else _string_from_file(infile)
+    keysize = len(key)
+    iv = iv * keysize  # TODO mod iv
+    text = base64.b64decode(text) if is_b64 else text
+    prev_block = None
+    modtext = b''
 
-	if action == "encrypt":
-		for group in grouper(text, keysize):
-			if not prev_block:
-				temp_block = xor_two_buffers_mod(group, iv)
-				prev_block = aes_128_in_ecb_mode(temp_block, key, "encrypt", from_string=True, from_b64=False)
-				modtext = modtext + prev_block
-			else:
-				filtered_group = [0 if i is None else i for i in group]
-				prev_block = xor_two_buffers_mod(prev_block, filtered_group)
-				modtext = modtext + aes_128_in_ecb_mode(prev_block, key, "encrypt", from_string=True, from_b64=False)
-		return modtext
+    if action == "encrypt":
+        for group in grouper(text, keysize):
+            if not prev_block:
+                temp_block = xor_two_buffers_mod(group, iv)
+                prev_block = aes_128_in_ecb_mode(temp_block, key, "encrypt", from_string=True, from_b64=False)
+                modtext = modtext + prev_block
+            else:
+                filtered_group = [0 if i is None else i for i in group]
+                prev_block = xor_two_buffers_mod(prev_block, filtered_group)
+                lol_block = aes_128_in_ecb_mode(prev_block, key, "encrypt", from_string=True, from_b64=False)
+                modtext = modtext + lol_block
+                prev_block = lol_block
+        return modtext
 
-	elif action == "decrypt":
-		for group in grouper(text, keysize):
-			if not prev_block:
-				prev_block = bytes(group)
-				temp_block = aes_128_in_ecb_mode(prev_block, key, "decrypt", from_string=True, from_b64=False)
-				plain_block = xor_two_buffers_mod(temp_block, iv)
-				modtext = modtext + plain_block
-			else:
-				filtered_group = bytes([0 if i is None else i for i in group])
-				temp_block = aes_128_in_ecb_mode(filtered_group, key, "decrypt", from_string=True, from_b64=False)
-				plain_block = xor_two_buffers_mod(temp_block, prev_block)
-				modtext = modtext + plain_block
-		return modtext
+    elif action == "decrypt":
+        for group in grouper(text, keysize):
+            if not prev_block:
+                prev_block = bytes(group)
+                temp_block = aes_128_in_ecb_mode(prev_block, key, "decrypt", from_string=True, from_b64=False)
+                plain_block = xor_two_buffers_mod(temp_block, iv)
+                modtext = modtext + plain_block
+            else:
+                filtered_group = bytes([0 if i is None else i for i in group])
+                temp_block = aes_128_in_ecb_mode(filtered_group, key, "decrypt", from_string=True, from_b64=False)
+                plain_block = xor_two_buffers_mod(temp_block, prev_block)
+                modtext = modtext + plain_block
+                prev_block = filtered_group
+        return modtext
 
-	else:
-		return
+    else:
+        return
--- test_cryptopals.py
+++ test_cryptopals.py
@@ -2,118 +2,124 @@ import cryptopals
 import base64
 import solutions
 
-def equals(a,b):
-	if a == b:
-		return True
-	else:
-		return False
+
+def equals(a, b):
+    if a == b:
+        return True
+    else:
+        return False
 
 # Problem 1.1: Convert hex to base64
 assert equals(
-	cryptopals.hex_to_b64(
-		"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f6973"\
-		"6f6e6f7573206d757368726f6f6d"
-	),
-	b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    cryptopals.hex_to_b64(
+        "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f6973"\
+        "6f6e6f7573206d757368726f6f6d"
+    ),
+    b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 )
 
 # Problem 1.2: Fixed XOR
 assert equals(
-	cryptopals.xor_two_buffers(
-		"1c0111001f010100061a024b53535009181c",
-		"686974207468652062756c6c277320657965"
-	),
-	"746865206b696420646f6e277420706c6179"
+    cryptopals.xor_two_buffers(
+        "1c0111001f010100061a024b53535009181c",
+        "686974207468652062756c6c277320657965"
+    ),
+    "746865206b696420646f6e277420706c6179"
 )
 
 # Problem 1.3: Single-byte XOR cipher
 assert equals(
-	cryptopals.decode_single_byte_xor_cypher(
-		b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-	),
-	"Cooking MC's like a pound of bacon"
+    cryptopals.decode_single_byte_xor_cypher(
+        b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+    ),
+    "Cooking MC's like a pound of bacon"
 )
 
 # Problem 1.4: Detect single-character XOR
 assert equals(
-	cryptopals.detect_single_byte_xor_cypher("testfiles/4.txt"),
-	"Now that the party is jumping"
+    cryptopals.detect_single_byte_xor_cypher("testfiles/4.txt"),
+    "Now that the party is jumping"
 )
 
 # Problem 1.5: Implement repeating-key XOR
 cryptopals.repeating_key_xor_encrypt(
-	"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
-	"ICE"
+    "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
+    "ICE"
 )
 
 # Problem 1.6: Break repeating-key XOR
 assert equals(
-	cryptopals.hamming_distance(
-		"this is a test",
-		"wokka wokka!!!"
-	),
-	37
+    cryptopals.hamming_distance(
+        "this is a test",
+        "wokka wokka!!!"
+    ),
+    37
 )
 
-#cryptopals.decrypt_vigenere("6.txt")
+# Temporarily disabled since we're using brute force and it's time-consuming
+# cryptopals.decrypt_vigenere("6.txt")
 
 # key is "Terminator X: Bring the noise"
 # decrypted is lyrics to "Play that Funky Music"
 
-# Problem 1.7
+# Problem 1.7: AES in ECB mode
 assert equals(
-	cryptopals.aes_128_in_ecb_mode("testfiles/7.txt", "YELLOW SUBMARINE", "decrypt"),
-	solutions.soln_7
+    cryptopals.aes_128_in_ecb_mode("testfiles/7.txt", "YELLOW SUBMARINE", "decrypt"),
+    solutions.soln_7
 )
 
-#crypted = cryptopals.encrypt_cbc_mode(solutions.snake_wounded, "YELLOW SUBMARINE", b'\x00', from_string=True, is_b64=False)
 crypted = cryptopals.aes_128_in_ecb_mode(solutions.snake_wounded, "YELLOW SUBMARINE", "encrypt", from_string=True, from_b64=False)
 assert equals(
-	cryptopals.aes_128_in_ecb_mode(crypted, "YELLOW SUBMARINE", "decrypt", from_string=True, from_b64=False),
-	solutions.snake_wounded
+    cryptopals.aes_128_in_ecb_mode(crypted, "YELLOW SUBMARINE", "decrypt", from_string=True, from_b64=False),
+    solutions.snake_wounded
 )
 
-# Problem 1.8
+# Problem 1.8: Detect AES in ECB mode
 assert equals(
-	cryptopals.detect_aes_128_in_ecb_mode("testfiles/8.txt"),
-	solutions.soln_8
+    cryptopals.detect_aes_128_in_ecb_mode("testfiles/8.txt"),
+    solutions.soln_8
 )
 
-# Problem 2.1 (9)
+# Problem 2.1 (9): Implement PKCS#7 padding
 assert equals(
-	cryptopals.pkcs7_padding("YELLOW SUBMARINE", 20),
-	"YELLOW SUBMARINE\x04\x04\x04\x04"
+    cryptopals.pkcs7_padding("YELLOW SUBMARINE", 20),
+    "YELLOW SUBMARINE\x04\x04\x04\x04"
 )
 
-print(solutions.snake_wounded)
-
-# Problem 2.2 (10)
+# Problem 2.2 (10): Implement CBC mode
 cbc_snake = (
-	cryptopals.cbc_mode(
-		solutions.snake_wounded,
-		"YELLOW SUBMARINE",
-		b'\x00',
-		"encrypt",
-		from_string=True,
-		is_b64=False
-	)
+    cryptopals.cbc_mode(
+        solutions.snake_wounded,
+        "YELLOW SUBMARINE",
+        b'\x00',
+        "encrypt",
+        from_string=True,
+        is_b64=False
+    )
 )
-
-print(cbc_snake)
 
 decoded_cbc_snake = (
-	cryptopals.cbc_mode(
-		cbc_snake,
-		"YELLOW SUBMARINE",
-		b'\x00',
-		"decrypt",
-		from_string=True,
-		is_b64=False
-	)
+    cryptopals.cbc_mode(
+        cbc_snake,
+        "YELLOW SUBMARINE",
+        b'\x00',
+        "decrypt",
+        from_string=True,
+        is_b64=False
+    )
 )
 
-print(decoded_cbc_snake)
+assert equals(solutions.snake_wounded, decoded_cbc_snake)
 
-#print(cryptopals.decrypt_cbc_mode())
+assert equals(
+    cryptopals.cbc_mode(
+        "testfiles/10.txt", "YELLOW SUBMARINE", b'\x00', "decrypt"
+    ).decode('utf-8'),
+    solutions.soln_10
+)
 
-#print(cryptopals.decrypt_cbc_mode("testfiles/10.txt", "YELLOW SUBMARINE", b'\x00'))
+# TODO: Problem 2.3 (11): An ECB/CBC detection oracle
+
+# TODO: Problem 2.4 (12): Byte-at-a-time ECB decryption (Simple)
+
+# TODO: Problem 2.5 (13): ECB cut-and-paste
