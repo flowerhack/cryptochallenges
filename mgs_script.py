6aeacc16e9bdb40c3d61e840e8d098e358adaf14
flowerhack <flowerhack@azureye.net>
2014-10-17 03:27:01 -0400

    Silliness notification

--- mgs_script.py
+++ mgs_script.py
@@ -1,3 +1,12 @@
+"""
+This is very silly.  Basically, I wanted a convenient divisble-by-16-bytes
+chunk o' text to test one of the encryption schemes, and I had the MGS3 theme
+stuck in my head, so CLEARLY this meant it needed to be a line FROM THAT GAME
+and this fetched all the possible candidates for me.
+
+I will probably move it to a repo explicitly meant for silly things later.
+"""
+
 import re
 
 prog = re.compile("[A-Za-z]+\: ")
